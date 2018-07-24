19a2f61e4840e842cbbe71450dce5f0333e98b17
Alagappan.al <alalagappan007@gmail.com>
2018-07-24 10:04:34 +0530

    Update and rename vowel.py to vowel.c

--- vowel.py
+++
@@ -1,9 +0,0 @@
-a=input()
-c=['a','e','i','o','u','A','E','I','O','U']
-if((a>='a' and a<='z')or(a>='A' and a<='Z')):
- if(a in c):
-  print("vowels")
- else:
-  print("Consonants")
-else:
- print("Invalid")
